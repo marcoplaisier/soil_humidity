63806953c431c0f5e61ee6e0f47bf062b6965753
Marco Plaisier <mplaisier@bol.com>
2017-03-19 15:26:55 +0100

    Fix valueerror

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from telegraf.client import TelegrafClient
 
 telegraf_client = TelegrafClient(host='146.185.169.132', port=8092)
 
-
 serial_client = serial.Serial('/dev/ttyUSB0', baudrate=9600)
 
 
 def send_data(moisture=0, sensor='Unknown'):
-    try:
-        telegraf_client.metric('moisture', moisture, tags={'location': 'in_home', 'sensor': sensor})
-    except ValueError:
-        pass  # skips one or more datapoints if they cannot be parsed. Especially when the Rasp connect to a running Arduino
+    telegraf_client.metric('moisture', moisture, tags={'location': 'in_home', 'sensor': sensor})
 
 
 def recv_serial():
@@ -22,7 +18,10 @@ def recv_serial():
         raw_data = serial_client.readline()
         print(raw_data)
         data_string = raw_data.decode('utf-8').strip()
-        data = json.loads(data_string)
+        try:
+            data = json.loads(data_string)
+        except ValueError:
+            pass  # skips one or more datapoints if they cannot be parsed. Especially when the Rasp connect to a running Arduino
         yield data
         time.sleep(10)
 
